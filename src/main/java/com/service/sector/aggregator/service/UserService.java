97507f6f5f3c0538403b63cd05c636d31b6b6fd6
Siarhei Naralenkau <naralenkov2010@gmail.com>
2025-07-22 09:24:59 +0200

    SH-76 - Implement user details endpoint

--- src/main/java/com/service/sector/aggregator/service/UserService.java
+++ src/main/java/com/service/sector/aggregator/service/UserService.java
@@ -5,6 +5,7 @@ import com.service.sector.aggregator.data.dto.AppUserRequest;
 import com.service.sector.aggregator.data.dto.AppUserResponse;
 import com.service.sector.aggregator.data.dto.auth.AuthResponse;
 import com.service.sector.aggregator.data.dto.auth.LoginRequest;
+import com.service.sector.aggregator.data.entity.AppUser;
 
 public interface UserService {
     AppUserResponse register(AppUserRequest request);
@@ -12,4 +13,6 @@ public interface UserService {
     AuthResponse login(LoginRequest request);
 
     void sendCode(String phone);
+
+    AppUser getUserDetails(String token);
 }
